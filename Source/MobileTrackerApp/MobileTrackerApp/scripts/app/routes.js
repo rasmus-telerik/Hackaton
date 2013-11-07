f71de7da4890fcb0023a8146130dd804b23b13e3
Thomas-Telerik <thomas.andersen@telerik.com>
2013-11-07 21:40:45 +0100

    Only loading routes on show

--- Source/MobileTrackerApp/MobileTrackerApp/scripts/app/routes.js
+++ Source/MobileTrackerApp/MobileTrackerApp/scripts/app/routes.js
@@ -2,16 +2,20 @@
   var RouteViewModel,
 	pp = global.app = global.app || {};
 
-  var data = Everlive.$.data('Routes');
-  var query = new Everlive.Query();
-  //query.where().eq('Driver', el.User.Id).select("StartTime");
-  query.select("StartTime");
-  data.get(query).then(function (data) {
-    app.routeService.viewModel.routes = data.result;
-  });
+  
 
   RouteViewModel = kendo.data.ObservableObject.extend({
     routes: [],
+    beforeShow: function (event) { 
+      var data = Everlive.$.data('Routes');
+      var query = new Everlive.Query();
+      //query.where().eq('Driver', app.el.User.Id).Done().select("StartTime");
+      query.select("StartTime");
+      data.get(query).then(function (data) {
+        app.routeService.viewModel.routes = data.result;
+        app.routeService.viewModel.trigger("change", { field: "routes" });
+      });
+    },
     onActivateRoute: function (e) {
       global.app.drivingService.viewModel.getTasksForDriver(e.data.Id);
       window.location.href = "#tabstrip-driving";
