7d6b975bbab2ffc633ad815bd5494db4b4400f40
Rasmus Svendsen <rasmus.svendsen@telerik.com>
2013-11-07 18:01:02 +0100

    Add navigation button for Driver

--- Source/MobileTrackerApp/MobileTrackerApp/scripts/app/driving.js
+++ Source/MobileTrackerApp/MobileTrackerApp/scripts/app/driving.js
@@ -12,14 +12,18 @@
     getTasksForDriver: function (id) {
       var data = Everlive.$.data('Tasks');
       var query = new Everlive.Query();
-      query.where().eq('Route', id).done().select("Id", "Description", "Location", "TimeInMin");
-      //query.select("Id","Description", "Location", "TimeInMin");
+      query.where().eq('Route', id).done().select("Id", "Description", "Location", "TimeInMin", "OrderNo").order("OrderNo");
+
       data.get(query).then(function (data) {
         app.drivingService.viewModel.tasks = data.result;
         app.drivingService.viewModel.trigger("change", { field: "tasks" });
       });
-    }
+    },
 
+    onNavigateToCustomer: function (id) {
+      var geopoint = id.data.Location;
+      window.location.href = "https://maps.google.dk/maps?q=" + geopoint.latitude + "%2C" + +geopoint.longitude;
+    }
   });
 
   app.drivingService = {
