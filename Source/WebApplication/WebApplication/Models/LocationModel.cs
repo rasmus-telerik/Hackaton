e596809d356a5e80a1870fe4ebb0fae6e22522d1
bokovskii <bokovskii@gmail.com>
2013-11-08 18:37:31 +0200

    create a location now works

---
+++ Source/WebApplication/WebApplication/Models/LocationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class LocationModel
+    {
+        public string Customer { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string VisitTime { get; set; }
+    }
+}
