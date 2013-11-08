61133c229ca704f55700cb34878a1845b02c0cae
Unknowni <ivan.yaroslavov@gmail.com>
2013-11-08 09:53:43 +0200

    Users

--- Source/WebApplication/WebApplication/Controllers/RouteDispatchController.cs
+++ Source/WebApplication/WebApplication/Controllers/RouteDispatchController.cs
@@ -1,12 +1,29 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
-    [Authorize(Roles="Dispatcher")]
+    [Authorize(Roles = "Dispatcher")]
     public class RouteDispatchController : Controller
     {
+        readonly UsersContext context;
+        public UserProfile EntityUser
+        {
+            get
+            {
+                return this.context.UserProfiles.FirstOrDefault(u => u.UserName == this.User.Identity.Name);
+            }
+        }
+
+        public RouteDispatchController()
+        {
+            this.context = new UsersContext();
+        }
+
         public ActionResult All()
         {
+            // get the everlive user by using the id of the entity user
             return View();
         }
 
